e945f52e8f96b28b80683e472852a3b08213465f
David Murillo <dmz866@hotmail.com>
2019-06-27 21:01:25 -0500

    model

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,13 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { format } from 'path';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
   styleUrls: ['./heroe.component.css']
 })
-export class HeroeComponent implements OnInit {
+export class HeroeComponent implements OnInit
+{
+  heroe = new HeroeModel();
 
   constructor() { }
+
+  guardar(heroeForm: NgForm)
+  {
+    if(heroeForm.invalid)
+    {
+      return;
+    }
+
+    console.log(this.heroe);
+  }
 
   ngOnInit() {
   }
